1ad22e649c6a90ba8a532a0fb87ee46265600ce1
Maela22 <maela.legall22@gmail.com>
2019-12-02 14:23:43 +0100

    Classe Categorie

--- CAS ERGOSUM/Modeles/Categorie.cs
+++ CAS ERGOSUM/Modeles/Categorie.cs
@@ -8,5 +8,46 @@ namespace CAS_ERGOSUM.Methodes
 {
     class Categorie
     {
+        #region Attributs
+        public static ArrayList CollClasseCategorie = new ArrayList();
+
+        private int _code;
+        private string _libelle;
+        private ArrayList _lesJouets;
+        #endregion
+
+        #region Constructeurs
+        public Categorie (int leCode, string leLibelle) 
+        {
+            _code=leCode;
+            _libelle=leLibelle;
+
+            _lesJouets=new ArrayList();
+
+            Categorie.CollClasseCategorie.Add(this);
+        }
+        #endregion
+
+        #region Getter/Setter
+        
+        public int Code { get => _code; set => _code = value; }
+        public string Libelle { get => _libelle; set => _libelle = value; }
+        #endregion
+
+        #region Methodes
+        public int GetCode()
+        {
+            return Code;
+        } 
+
+        public string GetLibelle()
+        {
+            return Libelle;
+        }
+
+      
+        #endregion
+    
+
     }
 }
