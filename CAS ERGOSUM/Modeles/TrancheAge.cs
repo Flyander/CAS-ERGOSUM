be58c4f61f229165677206cc92b984e6786bd515
YohannFin <yohann.fin13@gmail.com>
2019-12-02 14:03:59 +0100

    Creation de la classe TrancheAge

--- CAS ERGOSUM/Modeles/TrancheAge.cs
+++ CAS ERGOSUM/Modeles/TrancheAge.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,26 @@ namespace CAS_ERGOSUM.Methodes
 {
     class TrancheAge
     {
+        #region Attributs
+        public static ArrayList CollClassTrancheAge = new ArrayList();
+        private int _code;
+        private int _ageMin;
+        private int _ageMax;
+        #endregion
+
+        #region Constructeurs
+        public TrancheAge(int code, int ageMin, int ageMax)
+        {
+            Code = code;
+            AgeMin = ageMin;
+            AgeMax = ageMax;
+        }
+        #endregion
+
+        #region Getters/Setters
+        public int Code { get => _code; set => _code = value; }
+        public int AgeMin { get => _ageMin; set => _ageMin = value; }
+        public int AgeMax { get => _ageMax; set => _ageMax = value; }
+        #endregion
     }
 }
