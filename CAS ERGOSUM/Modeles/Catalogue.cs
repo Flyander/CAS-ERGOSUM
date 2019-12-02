f68a5a1fef8d091da052283b7984977749cfafea
DanielLedoussal <ledoussaldaniel@gmail.com>
2019-12-02 14:12:37 +0100

    Creation de la classe catalogue

--- CAS ERGOSUM/Modeles/Catalogue.cs
+++ CAS ERGOSUM/Modeles/Catalogue.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,29 @@ namespace CAS_ERGOSUM.Methodes
 {
     class Catalogue
     {
+        #region Atribut
+        private int _annee;
+        public static ArrayList CollClasseCatalogue = new ArrayList();
+        private Dictionary<Jouet, int> dicoQuantite;
+        #endregion
+
+        #region Constructeurs
+        public Catalogue(int annee)
+        {
+            _annee = annee;
+            Catalogue.CollClasseCatalogue.Add(this);
+            this.DicoQuantite = new Dictionary<Jouet, int>();
+        }
+        #endregion
+
+        #region getter-setter
+        public int Annee { get => _annee; set => _annee = value; }
+        internal Dictionary<Jouet, int> DicoQuantite { get => dicoQuantite; set => dicoQuantite = value; }
+        #endregion
+
+
+        #region methode
+
+        #endregion
     }
 }
