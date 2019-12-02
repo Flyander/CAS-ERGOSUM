e7acacd357a3aa88d4783b0a49146c970654bb27
alan bivic <alan.bivic@hotmail.fr>
2019-12-02 14:50:35 +0100

    ecriture des methodes Getinfos Convient

--- CAS ERGOSUM/Modeles/Jouet.cs
+++ CAS ERGOSUM/Modeles/Jouet.cs
@@ -15,15 +15,22 @@ namespace CAS_ERGOSUM.Methodes
 
             private int _numéro;
             private string _libéllé;
+            private Categorie _lesCategories;
+            private TrancheAge _lesTranches;
 
         #endregion
 
         #region Constructeur
 
-        public Jouet(int numéro, string libéllé)
+        public Jouet(int numéro, string libéllé, Categorie lesCategories, TrancheAge lesTranches)
         {
             Numéro = numéro;
             Libéllé = libéllé;
+            Jouet.CollClasseJouet.Add(this);
+            _lesCategories = lesCategories;
+            _lesTranches = lesTranches;
+
+
         }
 
         #endregion
@@ -37,7 +44,26 @@ namespace CAS_ERGOSUM.Methodes
 
         #region Methodes
 
-        public
+        public string GetInfos()
+        {
+            return (_libéllé +";"+ this._lesCategories.GetLibelle() + ";" + this._lesTranches.AgeMax.ToString()+ ";" + this._lesTranches.AgeMin.ToString());
+        }
+        public bool Convient(int unAge)
+        {
+            if (unAge > _lesTranches.AgeMax)
+            {
+                return false;
+            }
+           if(unAge < _lesTranches.AgeMin)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
 
         #endregion
 
